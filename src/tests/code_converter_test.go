ce39b33144c9fd014cdaee3621576126c241c02e
Beatrice Beacco <beatrice.beacco@gmail.com>
2024-07-31 14:58:15 +0200

    feat: updated code converter tests

--- src/tests/code_converter_test.go
+++ src/tests/code_converter_test.go
@@ -70,4 +70,77 @@ func TestConverter(t *testing.T) {
 }
 
 // TODO: test 16 bit conversion
-func TestCInstructionComposition(t *testing.T) {}
+func TestCInstructionComposition(t *testing.T) {
+	converter := assembler.CodeConverter{}
+
+	t.Run("Addition C instruction converted correctly", func(t *testing.T) {
+		comp := "D+A"
+		dest := "D"
+		jump := ""
+		loadA := false
+		want := "1110000010010000"
+		got, err := converter.CInstructionToBinary(comp, dest, jump, loadA)
+		assertNotError(t, err)
+		assertEqualString(t, got, want)
+	})
+
+	t.Run("Subtraction C instruction converted correctly", func(t *testing.T) {
+		comp := "D-M"
+		dest := "D"
+		jump := ""
+		loadA := true
+		want := "1111010011010000"
+		got, err := converter.CInstructionToBinary(comp, dest, jump, loadA)
+		assertNotError(t, err)
+		assertEqualString(t, got, want)
+	})
+
+	t.Run("Assignation C instruction converted correctly", func(t *testing.T) {
+		comp := "D"
+		dest := "M"
+		jump := ""
+		loadA := false
+		want := "1110001100001000"
+		got, err := converter.CInstructionToBinary(comp, dest, jump, loadA)
+		assertNotError(t, err)
+		assertEqualString(t, got, want)
+	})
+
+	t.Run("Jump C instruction converted correctly", func(t *testing.T) {
+		comp := "0"
+		dest := ""
+		jump := "JMP"
+		loadA := false
+		want := "1110101010000111"
+		got, err := converter.CInstructionToBinary(comp, dest, jump, loadA)
+		assertNotError(t, err)
+		assertEqualString(t, got, want)
+	})
+
+	t.Run("Invalid comp returns error", func(t *testing.T) {
+		comp := "5"
+		dest := ""
+		jump := "JMP"
+		loadA := false
+		_, err := converter.CInstructionToBinary(comp, dest, jump, loadA)
+		assertError(t, err)
+	})
+
+	t.Run("Invalid dest returns error", func(t *testing.T) {
+		comp := "D"
+		dest := "AAA"
+		jump := ""
+		loadA := false
+		_, err := converter.CInstructionToBinary(comp, dest, jump, loadA)
+		assertError(t, err)
+	})
+
+	t.Run("Invalid jump returns error", func(t *testing.T) {
+		comp := "D"
+		dest := ""
+		jump := "JJJ"
+		loadA := false
+		_, err := converter.CInstructionToBinary(comp, dest, jump, loadA)
+		assertError(t, err)
+	})
+}
